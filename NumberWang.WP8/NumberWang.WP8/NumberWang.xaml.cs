419d9febfa24f5d72f5bb4f185330cc046d26b2e
Jon Rea <jon.rea@nokia.com>
2013-08-09 13:56:12 +0100

    Other: Added button

--- NumberWang.WP8/NumberWang.WP8/NumberWang.xaml.cs
+++ NumberWang.WP8/NumberWang.WP8/NumberWang.xaml.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.Phone.Controls;
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
 using NumberWang.Engine;
 
 namespace NumberWang
 {
     public partial class NumberWang : PhoneApplicationPage
     {
-        private NumberWangModel _numberWangModel = new NumberWangModel();
+        private readonly NumberWangModel _numberWangModel = new NumberWangModel();
 
         public NumberWang()
         {
             InitializeComponent();
+        }
+
+        private void OnPlay(object sender, RoutedEventArgs e)
+        {
+            bool result = _numberWangModel.IsNumberWang(69);
+
+            txtResult.Text = result ? "That's NumberWang!!" : "Not Numberwang";
         }
     }
 }
