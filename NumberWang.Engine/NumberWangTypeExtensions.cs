2b68ddc02c5b29744d22dd83f5e420b6161cebd9
Ben Buttigieg <benbtg@gmail.com>
2013-08-11 10:45:35 +0100

    Added more audio variety and increased changes of Numberwang

--- NumberWang.Engine/NumberWangTypeExtensions.cs
+++ NumberWang.Engine/NumberWangTypeExtensions.cs
@@ -4,6 +4,12 @@ namespace NumberWang.Engine
 {
     public static class NumberWangTypeExtensions
     {
+        static readonly string[] AudioFilesCorrect = { "ThatsNumberWang1.mp3", "ThatsNumberWang2.mp3" };
+        static readonly string[] AudioFilesIncorrect = { "Wrong.mp3", "Wrong1.mp3", "Wrong2.mp3", "Wrong3.mp3" };
+        static readonly string[] AudioFilesWangernumb = { "Wanganumb.mp3" };
+
+        static readonly string BaseAudioFolder = "/Assets/Audio/";
+
         public static string ToWangString(this NumberWangType numberWangType)
         {
             switch (numberWangType)
@@ -21,22 +27,26 @@ namespace NumberWang.Engine
 
         public static Uri ToWangAudioFile(this NumberWangType numberWangType)
         {
-            var rndm = new Random(DateTime.UtcNow.Millisecond);
-
             switch (numberWangType)
             {
                 case NumberWangType.IsNumberWang:
-                    if (rndm.Next(2) == 1)
-                        return new Uri("/Assets/Audio/ThatsNumberWang1.mp3", UriKind.Relative);
-                    else
-                        return new Uri("/Assets/Audio/ThatsNumberWang2.mp3", UriKind.Relative);
+                    return CreateRandomUriFromAudioFiles(AudioFilesCorrect);                    
                 case NumberWangType.IsWangerNum:
-                    return null; // TODO get more sound effects!
+                    return CreateRandomUriFromAudioFiles(AudioFilesWangernumb);
                 case NumberWangType.NotNumberWang:
-                    return null;
+                    return CreateRandomUriFromAudioFiles(AudioFilesIncorrect);                    
                 default:
                     throw new InvalidOperationException("Unknown NumberWangType");
             }
+        }
+
+        private static Uri CreateRandomUriFromAudioFiles(string[] audioFiles)
+        {
+            var rndm = new Random(DateTime.UtcNow.Millisecond);
+            
+            var filename = audioFiles[rndm.Next(audioFiles.Length)];
+                        
+            return new Uri(BaseAudioFolder + filename, UriKind.Relative);
         }
     }
 }
